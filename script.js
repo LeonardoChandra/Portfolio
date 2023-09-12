a1474f13c8d4094fcac6bcec3927e7e68cc1d0f9
candoruu <leonardochandra2001@gmail.com>
2023-09-12 23:08:36 +0700

    Initial Commit
    Initializing Portfolio Project

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  const menu = document.querySelector(".hamburger-links");
+  const icon = document.querySelector(".hamburger-icon");
+
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
