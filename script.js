c6a320da2a02639a828046481d8e5bc24b6a1df7
candoruu <leonardochandra2001@gmail.com>
2023-09-15 19:32:58 +0700

    Finished front-end of the web

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-function toggleMenu() {
-  const menu = document.querySelector(".hamburger-links");
-  const icon = document.querySelector(".hamburger-icon");
+const inputs = document.querySelectorAll(".input");
+const menu = document.querySelector(".hamburger-links");
+const icon = document.querySelector(".hamburger-icon");
 
+function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
