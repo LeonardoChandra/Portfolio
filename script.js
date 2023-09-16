1f366f0f37cec5c84bebc869d62663cd011345d9
candoruu <leonardochandra2001@gmail.com>
2023-09-16 22:40:04 +0700

    Functional email form and some design tweaking

--- script.js
+++ script.js
@@ -23,3 +23,23 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
+
+function sendEmail() {
+  var params = {
+    Name: document.getElementById("nameID").value,
+    Email: document.getElementById("emailID").value,
+    Message: document.getElementById("messageID").value,
+  };
+
+  var serviceID = "service_aox5hfb";
+  var templateID = "template_xua3gif";
+
+  if (params.Name.value !== "" || params.Email.value !== "" || params.Message.value !== "") {
+    emailjs
+      .send(serviceID, templateID, params)
+      .then((res) => {
+        alert("Thank you, " + params["Name"] + "! Your message has been sent.");
+      })
+      .catch();
+  }
+}
